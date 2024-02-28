462264308aa3fbfc66f058639e6f4073a01cd056
Blake Morgan <blakemorgan@gmail.com>
2024-02-28 01:44:36 -0600

    Began Other projects page

--- src/pages/OtherProjects.js
+++ src/pages/OtherProjects.js
@@ -1,6 +1,16 @@
-
+import MediaPic from '../files/MediaAppPicture.PNG'
 function OtherProjects(){
-    return <div className="text-lg">Other Projects</div>
+    return <div className="bg-gray-300 ">
+        <h2 className="flex text-2xl justify-center items-center font-semibold ">Other projects</h2>
+            <h3 className="text-lg font-semibold">Projects during React Udemy Course</h3>
+                <h4 className='font-semibold'>User Photo Album App:</h4>
+                    <p className='text-base'>This is an app that lets you add random named users using faker. It then allows a photo album to be added to each user using faker to generate an album name. Then in each album you can add random photos (using faker). It stores all of this in a json-server database. And updates when new items are added to the server. This functionality is primarily done by redux, however for the users I used thunks rather than builder mutations. You are also able to delete users, photos, and albums.     </p>
+                    <p className='text-base'>Git Hub repo: <a className="text-blue-800" href="https://github.com/blake-a-morgan/media"> https://github.com/blake-a-morgan/media</a> </p>
+                    <img  src={MediaPic} alt='Media App' />
+                    <br></br>
+                <h4 className='font-semibold'>Books App:</h4>
+
+    </div>
 }
 
 export default OtherProjects;
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,7 +1,7 @@
 
 function ResumePage(){
     return <div className="pr-5 pl-5 items-center bg-gray-300">
-        <h2 className="items-center text-2xl">Resume:</h2>
+        <h2 className="items-center text-2xl font-semibold">Resume:</h2>
         <p>This will update every time I update it live.</p>
         <div className="flex items-center justify-center">
             <iframe className="items-center justify-center" title="Resume" height={1620} width={1080} src="https://docs.google.com/document/d/1C7nrSqRPDRoUKG7BvYBsOeeCVPuHXjmo" alt="My resume" />
